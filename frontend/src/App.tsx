c283178ad92f223d213c30265a3bcb9f80f41138
stepazel <stepan.zelenka@seznam.cz>
2021-08-04 21:49:59 +0200

    frontend and backend folders

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Zdar ne
+          je docela sranda tady psat
+      </h1>
+    </div>
+  );
+}
+
+export default App;
